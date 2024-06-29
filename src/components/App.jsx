0da54a90bf27f82448c7da607ab7b0610c64dc27
AndriyV2504 <andreyvolyanik@gmail.com>
2024-06-29 20:58:50 +0300

    add-userdata

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
 import Profile from "./Profile/Profile";
+import userData from "../userData.json";
 
 const App = () => {
   return (
     <main>
-    <Profile />;
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        atats={userData.stats}
+      />
     </main>
   );
 };
