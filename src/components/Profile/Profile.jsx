f4020f81fa2bc5d52cf28620bfeb7de140fd6b03
AndriyV2504 <andreyvolyanik@gmail.com>
2024-06-30 16:15:27 +0300

    add hw 1 end

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,15 +11,15 @@ function Profile({ name, tag, location, image, stats }) {
       </div>
 
       <ul className={styles.stats}>
-        <li className={styles.statsLi}>
+        <li className={styles.statsList}>
           <span className={styles.label}>Followers</span>
           <span className={styles.quantity}>{stats.followers}</span>
         </li>
-        <li className={styles.statsLi}>
+        <li className={styles.statsList}>
           <span className={styles.label}>Views</span>
           <span className={styles.quantity}>{stats.views}</span>
         </li>
-        <li className={styles.statsLi}>
+        <li className={styles.statsList}>
           <span className={styles.label}>Likes</span>
           <span className={styles.quantity}>{stats.likes}</span>
         </li>
