6366517afa6bb897035f893037f6ae03d6f5e522
AndriyV2504 <andreyvolyanik@gmail.com>
2024-06-30 19:15:26 +0300

    add-hw-1-friend-styles

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,14 +1,15 @@
 import FriendListItem from "./FriendListItem";
+import styles from "./FriendListItem.module.css";
 
 const FriendList = ({ friends }) => {
   return (
-    <ul>
+    <div className={styles.friendList}>
       {friends.map((friend) => (
         <li key={friend.id}>
           <FriendListItem {...friend} />
         </li>
       ))}
-    </ul>
+    </div>
   );
 };
 
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,10 +1,12 @@
+import styles from "./FriendListItem.module.css";
+
 const FriendListItem = ({ avatar, name, isOnline }) => {
   const statusStyle = {
     color: isOnline ? "green" : "red",
   };
 
   return (
-    <div>
+    <div className={styles.friendCard}>
       <img src={avatar} alt={`Avatar of ${name}`} width="48" />
       <p>{name}</p>
       <p style={statusStyle}>{isOnline ? "Online" : "Offline"}</p>
